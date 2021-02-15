e358e592f6e0bf911949d5005b890abc6331cb5d
Yagiz94 <yagizgani94@gmail.com>
2021-02-15 22:48:32 +0300

    Delete OSGI_Calculator directory

--- OSGI_Calculator/Converter/src/converter/service/Converter.java
+++
@@ -1,8 +0,0 @@
-package converter.service;
-
-public interface Converter {
-	public String numberToTextENG(int number);
-	public String numberToTextTR(int number);
-	public int textToIntENG(String inputText);
-	public int textToIntTR(String inputText);
-}
--- OSGI_Calculator/Converter/src/converter/utils/ConverterUtils.java
+++
@@ -1,5 +0,0 @@
-package converter.utils;
-
-public class ConverterUtils {
-
-}
